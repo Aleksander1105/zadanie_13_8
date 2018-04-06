f32319f5dc9d98ea94fa55e2b7021eee61e62684
Aleksander Matyjewicz <matyjewiczaleksander@gmail.com>
2018-04-06 12:03:38 +0200

    working on the project

---
+++ index.js
@@ -0,0 +1,23 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer();
+
+server.on('request', function (request, response) {
+    response.setHeader("Content-Type", "text/html; charset=utf-8");
+    if (request.method === 'GET' && request.url === '/hello') {
+        fs.readFile('./index.html', 'utf-8', function(err, data) {
+            if (err) throw err;
+        	console.log('Mission completed');
+        	response.write(data);
+        	response.end();
+        })
+    } else {
+        response.statusCode = 404;
+        response.write('<h1>404: Zła ścieżka!</h1>');
+        response.write("<img src='./f_u.jpg'>");
+        response.end();
+    }
+});
+
+server.listen(8080);
